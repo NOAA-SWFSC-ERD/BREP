a17de011f02b619d99dce0985d4cfb15613a7ebd
HeatherWelch <heather.welch@noaa.gov>
2017-06-05 10:59:31 -0700

    Code to take monthly rasters and search for highly correlated areas

---
+++ code/priority_areas.R
@@ -0,0 +1,6 @@
+## Code to take monthly rasters and search for highly correlated areas
+# follows corellation_DF.R
+
+#1. for each monthly raster (NA and zeros, n=24), turn binary for each of >.75, >.8, >.9
+#2. for each month, find common areas for each binary iteration
+#3. map out
