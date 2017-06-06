ada483eb8eb698babd194aa54d272af531a3c0c8
HeatherWelch <heather.welch@noaa.gov>
2017-06-06 11:06:20 -0700

    enso correlation code

---
+++ code/ENSO.R
@@ -0,0 +1,7 @@
+## checking SST box again ENSO index
+# index downloaded from: http://www.cpc.ncep.noaa.gov/products/analysis_monitoring/ensostuff/ONI_change.shtml
+
+
+##1. read in ENSO index
+
+ENSO=read.table("/Volumes/SeaGate/BREP/BREP/ENSO/detrend.nino34.ascii.txt",header = T)
